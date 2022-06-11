52f22c3cd8d09404a0001206be4001b3993ce34f
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2022-06-11 19:29:23 +0200

    Renamed scenes

--- Assets/_Scripts/OnSceneEnter.cs
+++ Assets/_Scripts/OnSceneEnter.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class OnSceneEnter : MonoBehaviour
 {
+    [SerializeField] GameObject military;
+    [SerializeField] GameObject terrorists;
+    [SerializeField] GameObject special;
+
     void Start()
     {
+        switch (PlayActionPlan.playerCount)
+        {
+            case 1:
+                break;
+            case 2:
+                terrorists.SetActive(true);
+                break;
+            case 3:
+                terrorists.SetActive(true);
+                special.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
         FindObjectOfType<PlayActionPlan>().allUnits = new List<Unit>();
         FindObjectOfType<PlayActionPlan>().FindUnits();
     }
